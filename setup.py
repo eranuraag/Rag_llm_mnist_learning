f2dee44e957c3d24713c598b801d8ced98ab114e
eranuraag <er.anuragsingh23@outlook.com>
2025-02-04 17:27:06 +0530

    setup file updated

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Build RAG and multi modal search  ",
-    version="0.0.0",
-    author="Anurag",
-    author_email="er.anuragsingh23@outlook.com",
-    packages=find_packages()
+    name="Build_RAG_MultiModal_Search",  # No special characters or double dashes
+    version="0.0.1",
 )
